f6fcd8ce1ea9ce81ed864ca584f4778438c6dbd3
Aleksandr <alex-davydov211@mail.ru>
2022-11-03 16:43:28 +0300

    add: needed files

---
+++ src/components/v-cart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="v-cart">
+        <p>Cart</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "v-cart",
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style>
+
+</style>
