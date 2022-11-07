bcdc29eb6476d3ffad653de57920549e6631501e
Aleksandr <alex-davydov211@mail.ru>
2022-11-07 20:31:22 +0300

    feat: cart, add item to cart, delete item, quantity

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
     <h1>Catalog</h1>
     <div class="v-catalog__list">
       <v-catalog-item v-for="product in PRODUCTS" :key="product.article" :product_data="product"
-        @sendArticle="showChildArticleInConsole" />
+        @addToCart="addToCart" />
     </div>
   </div>
 </template>
@@ -28,10 +28,11 @@ export default {
   },
   methods: {
     ...mapActions([
-      'GET_PRODUCTS_FROM_API'
+      'GET_PRODUCTS_FROM_API',
+      'ADD_TO_CART'
     ]),
-    showChildArticleInConsole(data) {
-      console.log(data);
+    addToCart(data) {
+      this.ADD_TO_CART(data);
     }
   },
   mounted() {
