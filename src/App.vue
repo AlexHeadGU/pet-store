55bff581b4e4653194f15f783f3a513f03f71a1b
Aleksandr <alex-davydov211@mail.ru>
2022-11-09 22:26:39 +0300

    feat: select option

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-    <div id="app">
-        <v-main-wrapper />
-    </div>
+  <div id="app">
+    <v-select :options="options" @selectOption="selectOption" :selected="selected" />
+    <p>Selected option goes here: {{ selected }}</p>
+    <v-main-wrapper />
+  </div>
 </template>
 
 <script>
-import vMainWrapper from './components/v-main-wrapper.vue';
+import vMainWrapper from './components/v-main-wrapper';
+import vSelect from './components/v-select';
 
 export default {
-    name: 'app',
-    components: {
-        vMainWrapper,
-    },
+  name: 'app',
+  components: {
+    vMainWrapper,
+    vSelect,
+  },
+  data() {
+    return {
+      options: [
+        { name: 'Option 1', value: 1 },
+        { name: 'Option 2', value: 2 },
+        { name: 'Option 3', value: 3 },
+        { name: 'Option 4', value: 4 },
+        { name: 'Option 5', value: 5 },
+      ],
+      selected: 'Select'
+    }
+  },
+  methods: {
+    selectOption(option) {
+      this.selected = option.name
+
+    }
+  }
 };
 
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
