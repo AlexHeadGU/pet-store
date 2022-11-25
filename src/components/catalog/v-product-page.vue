3dc10054b3fc6a0a7779b3ffa1d26c7d904e2638
Aleksandr <alex-davydov211@mail.ru>
2022-11-25 14:01:37 +0300

    fix: new tab for product

--- src/components/catalog/v-product-page.vue
+++ src/components/catalog/v-product-page.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="v-product-page">
-    <!-- <img class="v-catalog-item__image" :src="require('../../assets/images/' + product.image)" alt="img"> -->
-    <p>{{ product.image }}</p>
+    <img v-if="product.image" class="v-catalog-item__image" :src="require('../../assets/images/' + product.image)"
+      alt="img">
     <p>Product name: {{ product.name }}</p>
     <p>Article: {{ product.article }}</p>
-    <p>Price: {{ product.price }}</p>
+    <p v-if="product.price">Price: {{ formattedPrice }}</p>
+    <button class="v-catalog-item__add_to_cart_btn btn" @click="addToCart">Add to cart</button>
   </div>
 </template>
 
@@ -29,12 +30,21 @@ export default {
         }
       })
       return result
-    }
+    },
+    formattedPrice() {
+      let parts = this.product.price.toFixed(2).toString().split('.');
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+      return parts.join(".") + ' ₽';
+    },
   },
   methods: {
     ...mapActions([
-      'GET_PRODUCTS_FROM_API'
-    ])
+      'GET_PRODUCTS_FROM_API',
+      'ADD_TO_CART'
+    ]),
+    addToCart() {
+      this.ADD_TO_CART(this.product)
+    },
   },
 
   mounted() {
